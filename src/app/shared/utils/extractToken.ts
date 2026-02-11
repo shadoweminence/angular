9b4e88f275ef6edbae7f1985c1590462a07e6b26
Prashant Pokhrel <prashant.pokharel@truenary.com>
2026-02-11 11:32:23 +0545

    chore: removed unnecessary files

--- src/app/shared/utils/extractToken.ts
+++
@@ -1,5 +0,0 @@
-export const extractToken = (token: string | null): string | null => {
-  if (!token) return null;
-  const parts = token.split(' ');
-  return parts.length === 2 ? parts[1] : token;
-};
